eba7d27957bfad392268a19113d65d1af8285912
Matthew H. Irby <mirby7@gatech.edu>
2021-05-23 23:19:48 -0400

    Initialize blank project

---
+++ SpriteKitDemo/SpriteKitDemo/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  SpriteKitDemo
+//
+//  Created by Matthew H. Irby on 5/23/21.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+			
+    }
+	
+}
